ce90dd3b658ab0e17b67c540bddb33c8aa5e017e
Pyae Sone Aung <pyaesoneaung.code@gmail.com>
2023-07-31 11:05:46 +0630

    use suggest at ssh key

--- app/Concerns/CanAskSshKey.php
+++ app/Concerns/CanAskSshKey.php
@@ -2,15 +2,18 @@
 
 namespace App\Concerns;
 
-use function Laravel\Prompts\text;
+use function Laravel\Prompts\suggest;
 
 trait CanAskSshKey
 {
     public function askSshKey(string $default = '')
     {
-        return text(
+        return suggest(
             label: 'What is your ssh key path? (Optional)',
             placeholder: 'E.g. ~/.ssh/id_rsa',
+            options: [
+                '~/.ssh/id_rsa',
+            ],
             default: $default
         );
     }
